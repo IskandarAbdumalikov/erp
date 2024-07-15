a159c2464f7c5f7f7b7599091444de465e164c7a
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-07-15 17:53:39 +0500

    added

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -13,10 +13,8 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = signIn({ username, password });
-      if (isSuccess) {
-        navigate("/admin/customers");
-        localStorage.setItem("x-auth-token", data.token);
-      }
+      navigate("/admin/customers");
+      localStorage.setItem("x-auth-token", data.token);
     } catch (err) {
       console.error("Failed to login:", err);
     }
@@ -51,7 +49,9 @@ const Login = () => {
         <button type="submit" disabled={isLoading}>
           {isLoading ? "Logging in..." : "Login"}
         </button>
-        <Link style={{color:"blue"}} to={"/register"}>You have not an account? Register</Link>
+        <Link style={{ color: "blue" }} to={"/register"}>
+          You have not an account? Register
+        </Link>
       </form>
     </div>
   );
