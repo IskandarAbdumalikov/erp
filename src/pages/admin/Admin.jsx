12457fdd7f9876073d526625b7b9c6e9d3ae9290
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-07-17 08:50:33 +0500

    added

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -3,10 +3,12 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./admin.scss";
 import AdminHeader from "./admin-header/AdminHeader";
+import { useGetCustomersQuery } from "../../context/customerApi";
 
 const Admin = () => {
   const [close, setClose] = useState(false);
-  
+  let { data } = useGetCustomersQuery();
+  console.log(data);
 
   return (
     <div className={`admin ${close ? "close" : ""}`}>
